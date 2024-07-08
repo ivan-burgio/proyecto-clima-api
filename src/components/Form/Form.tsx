a1d9e97dfb4155529fb81f6826e2fa660491f48d
ivan-burgio <ivan.24.burgio@gmail.com>
2024-07-08 12:30:08 -0300

    dwageawd

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,35 +4,39 @@ import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
 import Alert from "../Alert/Alert";
 
-export default function Form() {
+type FormProps = {
+    fetchWeather: (search: SearchType) => Promise<void>;
+};
+
+export default function Form({fetchWeather} : FormProps) {
     const [search, setSearch] = useState<SearchType>({
         city: "",
         country: "",
     });
 
-    const [alert, setAlert] = useState('');
+    const [alert, setAlert] = useState("");
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+    ) => {
         setSearch({
             ...search,
-            [e.target.name] : e.target.value
-        })
-    }
+            [e.target.name]: e.target.value,
+        });
+    };
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if(Object.values(search).includes('')) {
-            setAlert('Todos los campos son obligatorios');
+        if (Object.values(search).includes("")) {
+            setAlert("Todos los campos son obligatorios");
             return;
         }
-    }
+        fetchWeather(search);
+    };
 
     return (
-        <form
-            className={styles.form}
-            onSubmit={handleSubmit}
-        >
+        <form className={styles.form} onSubmit={handleSubmit}>
             {alert && <Alert>{alert}</Alert>}
 
             <div className={styles.field}>
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { SearchType } from "../types";
+
+export default function useWeather() {
+    const fetchWeather = async (search: SearchType) => {
+        const appId = import.meta.env.VITE_API_KEY;
+
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+            const { data } = await axios(geoUrl);
+            
+            const lat = data[0].lat
+            const lon = data[0].lon
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+            const { data: weatherResult } = await axios(weatherUrl);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return {
+        fetchWeather,
+    };
+}
