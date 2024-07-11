33435f6001eafa70b92a611ce636e7ee2c0ce0e0
ivan-burgio <ivan.24.burgio@gmail.com>
2024-07-11 11:10:13 -0300

    fin 2

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -35,7 +35,7 @@ export default function useWeather() {
         setWeather(initialState);
 
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
             const { data } = await axios(geoUrl);
 
             // Comprobar si existe
